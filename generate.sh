fa9774cd26f93aedbb6f27e0364687c2fd799f26
Hugo <hugovk@users.noreply.github.com>
2018-01-09 15:16:38 +0200

    Run for real

--- generate.sh
+++ generate.sh
@@ -4,4 +4,4 @@
 set -e
 
 # Generate the files
-/usr/local/bin/pypinfo --test --json --limit 500 --days 365 "" project > top-pypi-packages.json
+/usr/local/bin/pypinfo --json --limit 500 --days 365 "" project > top-pypi-packages.json
