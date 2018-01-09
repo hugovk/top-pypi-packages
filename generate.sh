b0730a0fdd8f93b5a6b401c0284f8d55beeb8b6e
Hugo <hugovk@users.noreply.github.com>
2018-01-09 14:32:37 +0200

    Test call of pypinfo

--- generate.sh
+++ generate.sh
@@ -3,8 +3,7 @@
 # Prevents script from running if there are any errors
 set -e
 
-# Generate the files
-# pypinfo --test --json --limit 500 --days 365 "" project > top-pypi-packages.json
-# TESTING
 echo "$(date)"
-echo "$(date)" > top-pypi-packages.json
+
+# Generate the files
+pypinfo --test --json --limit 500 --days 365 "" project > top-pypi-packages.json
