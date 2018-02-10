26ed5a34a6262e5aa7f9b28749767f04d22f88ef
Hugo <hugovk@users.noreply.github.com>
2018-02-10 11:21:32 +0200

    Indent JSON with 0 spaces, but use newlines

--- generate.sh
+++ generate.sh
@@ -4,5 +4,5 @@
 set -e
 
 # Generate the files
-/usr/local/bin/pypinfo --json --limit 5000 --days  30 "" project > top-pypi-packages-30-days.json
-/usr/local/bin/pypinfo --json --limit 5000 --days 365 "" project > top-pypi-packages-365-days.json
+/usr/local/bin/pypinfo --json --indent 0 --limit 5000 --days  30 "" project > top-pypi-packages-30-days.json
+/usr/local/bin/pypinfo --json --indent 0 --limit 5000 --days 365 "" project > top-pypi-packages-365-days.json
