be147aee71a373fb657c7cb314c8f07b7270efa8
Xie Yanbo <xieyanbo@gmail.com>
2020-05-25 15:43:11 +0800

    fetch total number of projects

--- generate.sh
+++ generate.sh
@@ -3,9 +3,24 @@
 # Prevents script from running if there are any errors
 set -e
 
+# Fetch total number of packages
+TOTLAL=$(curl -s https://pypi.org \
+  | sed -nE -e '/[,0-9]+ projects/p' \
+  | sed -e 's/projects//g' -e 's/[ ,]//g')
+
+add_total_number()
+{
+  jq --arg total_projects $TOTAL '{total_projects: $total_projects}+.' \
+    | sed -E -e 's/^ +//g'
+}
+
 # Generate the files
-/usr/local/bin/pypinfo --json --indent 0 --limit 4000 --days  30 "" project > top-pypi-packages-30-days.json
-/usr/local/bin/pypinfo --json --indent 0 --limit 4000 --days 365 "" project > top-pypi-packages-365-days.json
+/usr/local/bin/pypinfo --json --indent 0 --limit 4000 --days  30 "" project \
+  | add_total_number \
+  > top-pypi-packages-30-days.json
+/usr/local/bin/pypinfo --json --indent 0 --limit 4000 --days 365 "" project \
+  | add_total_number \
+  > top-pypi-packages-365-days.json
 
 # Minify the files
 # Not human-readable or diffable, but about 11% smaller, eg. 268K->238K
