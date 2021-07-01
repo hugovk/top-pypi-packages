fa2ec4a2ecb27b6e91f9195e36b181b6134e91d5
Hugo van Kemenade <hugovk@users.noreply.github.com>
2021-07-01 17:12:31 +0300

    Increase 30-day data from 4k to 5k packages

--- generate.sh
+++ generate.sh
@@ -16,9 +16,9 @@ python3 -m pip --version
 /usr/local/bin/pypinfo --version
 
 # Generate and minify for 30 days
-/usr/local/bin/pypinfo --json --indent 0 --limit 4000 --days  30 "" project > top-pypi-packages-30-days.json
+/usr/local/bin/pypinfo --json --indent 0 --limit 5000 --days  30 "" project > top-pypi-packages-30-days.json
 jq -c . < top-pypi-packages-30-days.json > top-pypi-packages-30-days.min.json
 
 # Generate and minify for 365 days
-#/usr/local/bin/pypinfo --json --indent 0 --limit 4000 --days 365 "" project > top-pypi-packages-365-days.json
+#/usr/local/bin/pypinfo --json --indent 0 --limit 5000 --days 365 "" project > top-pypi-packages-365-days.json
 #jq -c . < top-pypi-packages-365-days.json > top-pypi-packages-365-days.min.json
