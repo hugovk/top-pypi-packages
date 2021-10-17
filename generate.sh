f4f0de551020e51a98b76466a67f1584450b9cda
Hugo van Kemenade <hugovk@users.noreply.github.com>
2021-10-17 20:01:48 +0300

    Update path

--- generate.sh
+++ generate.sh
@@ -5,7 +5,7 @@ set -e
 
 # Check versions
 python3 -m pip --version
-/usr/local/bin/pypinfo --version
+/home/botuser/.local/bin/pypinfo --version
 
 # Ensure newest pip and pypinfo
 python3 -m pip install -U pip
@@ -13,12 +13,12 @@ python3 -m pip install -U pypinfo
 
 # Check versions
 python3 -m pip --version
-/usr/local/bin/pypinfo --version
+/home/botuser/.local/bin/pypinfo --version
 
 # Generate and minify for 30 days
-/usr/local/bin/pypinfo --json --indent 0 --limit 5000 --days  30 "" project > top-pypi-packages-30-days.json
+/home/botuser/.local/bin/pypinfo --json --indent 0 --limit 5000 --days  30 "" project > top-pypi-packages-30-days.json
 jq -c . < top-pypi-packages-30-days.json > top-pypi-packages-30-days.min.json
 
 # Generate and minify for 365 days
-#/usr/local/bin/pypinfo --json --indent 0 --limit 5000 --days 365 "" project > top-pypi-packages-365-days.json
+#/home/botuser/.local/bin/pypinfo --json --indent 0 --limit 5000 --days 365 "" project > top-pypi-packages-365-days.json
 #jq -c . < top-pypi-packages-365-days.json > top-pypi-packages-365-days.min.json
