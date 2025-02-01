56dc1c2f8b0784feb5ec7eb62f6327debea97fa9
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2025-02-01 11:16:06 +0200

    Fetch data for 15k PyPI packages (for #44)

--- build.sh
+++ build.sh
@@ -12,10 +12,6 @@ git pull origin main
 # Generate the files
 bash generate.sh
 
-# Remove big unzipped file
-rm top-pypi-packages-30-days-all.csv
-rm top-pypi-packages-30-days-all.json
-
 # Make output directory, don't fail if it exists
 # mkdir -p build
 
